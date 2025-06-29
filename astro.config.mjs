1084e3aef98d2d391d101955dd73d77614c2a206
Astrophysics108 <dsingh@perse.co.uk>
2025-06-29 13:50:59 +0100

    hoping it will deploy now

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://astrophysics108.github.io',
-    base: 'bye-mr-gwilt',
+    //base: 'bye-mr-gwilt',
   })
