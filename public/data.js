3280685825bea1f32aabdadd69d69c8a1002c76c
Astrophysics108 <dsingh@perse.co.uk>
2025-06-29 21:29:38 +0100

    IT WORKS NOW

--- public/data.js
+++ public/data.js
@@ -1,4 +1,6 @@
-fetch("https://script.google.com/macros/s/AKfycbz14-aYUBW-saYNaUNA-XtMN-U8BvKyDZRCHTLEYWUKNv5JoVt6wqSyXkNmxTfXjNiEQQ/exec")
+
+
+fetch("https://script.google.com/macros/s/AKfycbymNtqc_8ePorbxoyLem5GC616ycLg3zEi_Nt9WjOJmdtdGx6Imd4JiPMc4YvhYmOvdMg/exec")
 .then(response => response.json())
 .then(data => {
 const container = document.getElementById('responses');
