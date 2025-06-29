2911301eeb444891239fe7167c6325eb8b7d3acc
Astrophysics108 <dsingh@perse.co.uk>
2025-06-29 14:30:07 +0100

    IT WORKS IT FINALLY DEPLOYS OMG

---
+++ public/sidebar.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+  });
